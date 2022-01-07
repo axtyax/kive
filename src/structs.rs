5ac0b31251069bbca725cc4692132066969b9207
Darius Barbano <darius.barbano@gmail.com>
2022-01-07 13:27:54 -0500

    scoring

---
+++ src/structs.rs
@@ -0,0 +1,21 @@
+
+mod ts {
+
+    pub trait Term {
+        fn foo() -> f64;
+    }
+
+
+    pub trait Query {
+        fn foo() -> f64;
+    }
+
+    pub trait Document {
+        fn foo() -> f64;
+    }
+
+    pub trait Collection {
+        fn foo() -> f64;
+    }
+
+}
